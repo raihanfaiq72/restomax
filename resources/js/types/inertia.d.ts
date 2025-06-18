3d5fd4e31810c6898cbaa8c23dfab2a1f43e7113
IQ72 <105931001+raihanfaiq72@users.noreply.github.com>
2025-06-18 21:00:46 +0700

    fix: fixing bug on lint

--- resources/js/types/inertia.d.ts
+++ resources/js/types/inertia.d.ts
@@ -1,3 +1,33 @@
+// import { Page } from '@inertiajs/core';
+
+// interface User {
+//   id: number;
+//   name: string;
+//   email: string;
+// }
+
+// interface Flash {
+//   success?: string;
+//   error?: string;
+// }
+
+// interface InertiaProps {
+//   auth: {
+//     user: User | null;
+//   };
+//   flash: Flash;
+// }
+
+// declare module '@inertiajs/core' {
+//   interface PageProps extends InertiaProps {}
+// }
+
+// declare module 'vue' {
+//   interface ComponentCustomProperties {
+//     $page: Page<InertiaProps>;
+//   }
+// }
+
 import { Page } from '@inertiajs/core';
 
 interface User {
@@ -19,7 +49,9 @@ interface InertiaProps {
 }
 
 declare module '@inertiajs/core' {
-  interface PageProps extends InertiaProps {}
+  interface Page<Props extends InertiaProps = InertiaProps> {
+    props: Props;
+  }
 }
 
 declare module 'vue' {
