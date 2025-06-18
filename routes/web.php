1c959f6a1ab3a29f57791d75edade579efb4774b
IQ72 <105931001+raihanfaiq72@users.noreply.github.com>
2025-06-18 21:14:15 +0700

    fix : bug fix on lint

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Categories\CategoryController;
-use App\Http\Controllers\Product\ProductController;
-use App\Http\Controllers\Ingredients\IngredientsController;
-use App\Http\Controllers\Customer\CustomerController;
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -14,10 +10,6 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function(){
-   //
-});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
